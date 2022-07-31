d74711586f1252c0f4e8dc3c41775380f7b35e1e
Samuel De Oliveira <samuel.deoliveir1@gmail.com>
2022-07-31 04:54:01 +0200

    first push

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'react-native-gesture-handler'
+
+import {NavBar} from './lib/components/NavBar'
+
+export default function App() {
+  return (
+    <NavBar/>
+  );
+}
